7e1e9b4af7c7d451962c1a32417375c1fed090db
mingshashan <mingshashan@gmail.com>
2022-06-24 17:39:47 +0800

    update

--- dependency-lookup/src/main/java/com/mingshashan/practice/spring/ioc/denpendency/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/com/mingshashan/practice/spring/ioc/denpendency/lookup/ObjectProviderDemo.java
@@ -1,8 +1,11 @@
 package com.mingshashan.practice.spring.ioc.denpendency.lookup;
 
+import com.mingshashan.practice.spring.ioc.container.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Iterator;
 
 public class ObjectProviderDemo {
 
@@ -14,12 +17,34 @@ public class ObjectProviderDemo {
         applicationContext.refresh();
 
         lookupByObjectProvider(applicationContext);
+        lookupByIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
         applicationContext.close();
+    }
+
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+        Iterable<String> stringIterable = objectProvider;
+        for (String s : stringIterable) {
+            System.out.println(s);
+        }
+
+        objectProvider.stream().forEach(System.out::println);
+    }
+
+    private static void lookupByIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> objectProvider = applicationContext.getBeanProvider(User.class);
+        User user = objectProvider.getIfAvailable(User::createUser);
+        System.out.println(user);
     }
 
     @Bean
     public String hello() {
         return "Hello";
+    }
+    @Bean
+    public String message() {
+        return "messge";
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
